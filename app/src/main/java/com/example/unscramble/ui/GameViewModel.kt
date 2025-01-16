5bb3b43d58d5981dbf64862df6a07f1ce8488c55
Hongor <hongor0407@gmail.com>
2025-01-15 20:07:49 -0500

    you cannot play more than 10 words

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.MAX_NO_OF_WORDS
 import com.example.unscramble.data.SCORE_INCREASE
 import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -68,14 +69,25 @@ class GameViewModel : ViewModel() {
     }
 
     private fun updateGameState(updatedScore : Int){
-        _uiState.update { currentState ->
-            currentState.copy(
-                currentScrambledWord = pickRandomWordAndShuffle(),
-                score =  updatedScore,
-                isGuessedWrong = false,
-                currentWordCount = currentState.currentWordCount.inc()
+        if (usedWords.size == MAX_NO_OF_WORDS){
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
                 )
+            }
+        } else {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    currentScrambledWord = pickRandomWordAndShuffle(),
+                    score =  updatedScore,
+                    isGuessedWrong = false,
+                    currentWordCount = currentState.currentWordCount.inc()
+                )
+            }
         }
+
     }
 
     fun skipWord() {
