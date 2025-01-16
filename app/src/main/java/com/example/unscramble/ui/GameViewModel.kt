519dc4534081cbae0c9096cb6d3fac99dfd865c8
Hongor <hongor0407@gmail.com>
2025-01-15 19:35:36 -0500

    added gameviewmodel

---
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+
+
+    private lateinit var currentWord : String
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState : StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    private var usedWords : MutableSet<String> = mutableSetOf()
+
+    init {
+        resetGame()
+    }
+
+    private fun pickRandomWordAndShuffle() : String {
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord))
+            return pickRandomWordAndShuffle()
+        else{
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(currentWord : String) : String {
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+        while (tempWord.equals(currentWord))
+            tempWord.shuffle()
+        return String(tempWord)
+    }
+
+    fun resetGame(){
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+}
